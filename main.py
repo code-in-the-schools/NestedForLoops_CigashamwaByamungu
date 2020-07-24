bb29bd8e069a974e0abe91120ef237dd2dfb86c1
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-24 15:26:43 +0000

    Everything

---
+++ main.py
@@ -0,0 +1,23 @@
+#I dont know where to Start  
+
+#Outer Loop
+for i in range (0,6):
+  print("Outer Loop | i = " + str(i) )
+
+  #Inner Loop
+  for j in range (0, 6):
+    print("inner loop | i = " + str(i) + " | j = " + str(j))
+    
+    print(i, j)
+
+#Original Verison^^^^^^^Experiement Below
+
+ #Outer Loop
+for i in range (0,10):
+  print("Outer Loop | i = " + str(i) )
+
+  #Inner Loop
+  for j in range (0, 10):
+    print("inner loop | i = " + str(i) + " | j = " + str(j))
+    
+    print(i, j)
